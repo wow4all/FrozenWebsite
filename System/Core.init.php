9063121a7275a63b3e5fc566046f6888ac7d80d4
Asfo <asfo@flikes.net>
2011-11-28 07:54:53 -0600

    [14]Changelog:
    *Se modifica el archivo Core.init.php que ahora inicializará absolutamente todo el sistema llamando archivos necesarios para todo el funcionamiento de la web.
    *Se agrega seguridad al archivo Core.init.php para bloquear accesos directos.
    *Se elimina el archivo Core.Settings.php que antes se tenía planeado fuera quien iniciara las configuraciones adicionales que el sistema deberá marcar pero ahora se hará de forma diferente.

--- System/Core.init.php
+++ System/Core.init.php
@@ -4,13 +4,18 @@
  *                            -------------------
  *   Proyecto              : FrozenSocial
  *   Inicio                : Septiembre 28, 2011
- *   Última modificación   : Noviembre 07, 2011
+ *   Última modificación   : Noviembre 28, 2011
  *   Copyright             : (C) 2011 iAsfo & Frozen WoW
  *
  ***************************************************************************/
  
  	define('ROOT_PATH', str_replace('\\', '/',dirname(__FILE__)).'/');
-	
+	require 'Class/Class.Log.php';
+	if(!DEFINED('ASFO'))
+	{
+		Log::HackLog("Se intentó acceder directamente al archivo Core.init.php, pero se denegó el acceso", 'Core.init.php');
+		exit("<h1>Acceso directo</h1><p>Este archivo no puede ser visualizado directamente. Se ha bloqueado la navegaci&oacute;n</p><small>Su IP fue almacenada por cuestiones de seguridad</small>");
+	}
 	if(!isset($initalCore))
 	{
 		/*----------------------------------
@@ -21,24 +26,32 @@
 		*	Carga de definiciones y configuraciones
 		*----------------------------------*/
 		require 'Configs/Config.php';
+		/*----------------------------------
+		*	Carga de Smarty e inicialización
+		*----------------------------------*/
 		DEFINE('SMARTY_DIR', ROOT_PATH.'Smarty/');
-		if(file_exists(SMARTY_DIR.'Smarty.class.php'))
-			require SMARTY_DIR.'Smarty.class.php';
+		require SMARTY_DIR.'Smarty.class.php';
 		$Smarty = new Smarty();
-		//---//
-		require 'Class/Class.Log.php';
+		/*----------------------------------
+		*	Carga de las Clases del sistema
+		*----------------------------------*/
 		require 'Class/Class.Database.php';
-		require 'Class/Class.System.php';
-		require 'Class/Class.Shoutbox.php';
-		
+		/*----------------------------------
+		*	Conexión a la base de datos (única)
+		*----------------------------------*/
 		DataBase::Connection();
-
+		/*----------------------------------
+		*	Segunda Carga de las Clases del Sistema
+		*	*Estas requieren de las anteriores y se ordenan
+		*----------------------------------*/
 		require 'Class/Class.Wall.php';
 		require 'Class/Class.Register.php';
+		require 'Class/Class.System.php';
+		require 'Class/Class.Shoutbox.php';
 		/*----------------------------------
 		*	Carga de las configuraciones
 		*----------------------------------*/
-		require 'Core.Settings.php';
+		require 'Class/Class.Profile.php';
 		/*----------------------------------
 		*	Carga del template fuente
 		*----------------------------------*/
